01fd6259764f68e6e060fe8a38e94c3e6ab64bc1
tbswang <gt2017qd@163.com>
2021-06-15 12:25:14 +0800

    add btn

--- react-16.14.0/index.js
+++ react-16.14.0/index.js
@@ -1,5 +1,20 @@
 function App() {
-    return <div>Hello, React Source Code!</div>
+    return <div>Hello, React Source Code!
+      <AddBtn />
+    </div>
 }
 
 ReactDOM.render(<App/>, document.querySelector('#app'))
+
+function AddBtn(){
+  const [ count, setCount] = React.useState(0);
+  function handleClick(){
+    setCount(count +1);
+  }
+  return (
+    <div>
+    <div>{count}</div>
+    <button onClick={handleClick}>add</button>
+    </div>
+  );
+}
